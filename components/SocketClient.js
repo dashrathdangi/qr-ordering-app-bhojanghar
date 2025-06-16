1992d8226a4e1c4abd24f706f3c2c7e6a854636f
DASHRATH DANGI <dashrathdangi66@gmail.com>
2025-06-16 16:19:19 +0530

    🔧 Fix: use same-origin for Socket.IO connection

--- components/SocketClient.js
+++ components/SocketClient.js
@@ -39,7 +39,7 @@ const handleNewOrder = (orders) => {
     // Create socket connection once
     if (!socket) {
 
-  socket = io("https://bhojanghar.ap-south-1.elasticbeanstalk.com", {
+  socket = io(socketUrl, {
   transports: ['websocket'],
   withCredentials: true,
   path: "/api/socket",
